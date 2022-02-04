4876e6503669b52c6e0f476224871acd21bb0bc1
Alex Schnapps <6kwalitet@gmail.com>
2022-02-05 01:12:09 +0400

    add questions

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -12,16 +12,26 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    //MARK: - IBActions
-    @IBAction func answerButtomPressed(_ sender: UIButton) {
-    }
+    let quizz = ["A slug's blood is green.",
+                 "Approximately one quarter of human bones are in the feet.",
+                 "The total surface area of two human lungs is approximately 70 square metres."
+    ]
     
+    var questionNumber = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionLabel.text = "Four + Two is equal to Six"
+        updateUI()
     }
-
-
+    //MARK: - IBActions
+    @IBAction func answerButtomPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quizz[questionNumber]
+    }
+    
 }
 
