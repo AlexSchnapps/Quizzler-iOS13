1a25a80e8456761f1f1c336ace6dba0e5484ea8a
Alex Schnapps <6kwalitet@gmail.com>
2022-02-05 00:39:14 +0400

    add IBOutlets, IBActions.

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -1,18 +1,25 @@
 //
 //  ViewController.swift
 //  Quizzler-iOS13
-//
-//  Created by Angela Yu on 12/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
 
 import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - IBOutlets
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    //MARK: - IBActions
+    @IBAction func answerButtomPressed(_ sender: UIButton) {
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        questionLabel.text = "Four + Two is equal to Six"
     }
 
 
